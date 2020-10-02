4ff8c2f45613bf9d0b14e560d61730cb2531c42d
Fábio da Silva <fabinhoevil16@hotmail.com>
2020-10-01 22:53:59 -0300

    testando styled-components

--- App.js
+++ App.js
@@ -1,21 +1,29 @@
 import React from 'react';
 import { SafeAreaView,View, Text, StyleSheet } from 'react-native';
+import styled from 'styled-components/native';
+
+const Page = styled.SafeAreaView`
+  flex:1;
+  background-color:blue;
+`;
+
+const Texto = styled.Text`
+  color:${props=>props.cor};
+  font-size:30px;
+  background-color:black;
+
+`;
 
 export default () => {
   return (
-    <SafeAreaView style={styles.page}>
-      <Text>Olá Mundo</Text>
-    </SafeAreaView>
+    <Page>
+      <Texto cor="red">Texto De Exemplo</Texto>
+      <Texto cor="yellow">Texto De Exemplo</Texto>
+    </Page>
   );
 }
 
-const styles = StyleSheet.create({
-  page:{
-    width:200,
-    height:200,
-    backgroundColor:'#FF0000'
-  }  
-});
+
 
 
 
